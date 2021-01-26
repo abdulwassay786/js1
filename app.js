95fdca18873cccf46f83ae130e3a2687b47baee5
Abdul Wassay Ansari <abdulwassayansari786@gmail.com>
2021-01-26 22:53:25 +0500

    Calculator

--- app.js
+++ app.js
@@ -494,3 +494,19 @@
 
 // }
 // area(2,2)
+
+function getValue(e) {
+    a = document.getElementById('result')
+    a.value += e
+}
+
+function calculate() {
+    a = document.getElementById('result')
+    var final = eval(a.value)
+    a.value = final
+}
+
+function clearValue() {
+    var a = document.getElementById('result')
+    a.value = ''
+}
